fbd67d341520cf7ee86a0ca840a32b7b61074fa6
GGS825 <geguosheng@itis-molinari.eu>
2023-02-20 16:30:26 +0100

    NULL

---
+++ Studio/Libreria vector/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    
+    vector<int> v2;
+
+    int v[6];
+    for(int i=0;i<5,i++)
+    {
+       v[i]=2;
+    };
+
+     cout<<;
+    return 0;
+};
